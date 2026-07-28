ecfe0b6d939a02c1ac3284d0d6b5f89979502757
Paloma Bas Fernández <palomabasfer@gmail.com>
2026-07-28 13:54:25 +0200

    chore: initial project structure setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlops_pipeline",
+    version="0.1.0",
+    description="Production MLOps Pipeline with MLflow & Docker",
+    author="Paloma Bas Fernández",
+    packages=find_packages(),
+)
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+"""MLOps Pipeline Package."""
+
+__version__ = "0.1.0"
